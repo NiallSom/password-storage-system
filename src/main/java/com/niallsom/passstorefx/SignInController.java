88e5431998732b0880f399ba1a2ab4e94730e794
Niall <niallsomers205@gmail.com>
2023-11-20 20:00:30 +0000

    Changed UI

--- src/main/java/com/niallsom/passstorefx/SignInController.java
+++ src/main/java/com/niallsom/passstorefx/SignInController.java
@@ -7,18 +7,16 @@ import javafx.scene.control.TextField;
 
 public class SignInController {
     public static String userToken;
-    @FXML
-    private Button signUp;
+
     @FXML
     private Button login;
-
     @FXML
     private TextField username;
     @FXML
     private TextField password;
     Alert alert = new Alert(Alert.AlertType.INFORMATION);
     public void initialize(){
-        signUp.setOnAction((e)-> SceneController.setScene(e,"sign_up_view"));
+        //signUp.setOnAction((e)-> SceneController.setScene(e,"sign_up_view"));
         login.setOnAction((e)->{
             if (DatabaseHandling.signIn(username.getText(),password.getText())){
                 userToken = password.getText();// set user token to the login password so stored data can be decrypted
