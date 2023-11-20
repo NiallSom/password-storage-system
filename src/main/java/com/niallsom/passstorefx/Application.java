66f921c541328fbd319001aed3425e72a6b15468
Niall <niallsomers205@gmail.com>
2023-11-20 20:12:38 +0000

    Added Icon

--- src/main/java/com/niallsom/passstorefx/Application.java
+++ src/main/java/com/niallsom/passstorefx/Application.java
@@ -2,6 +2,7 @@ package com.niallsom.passstorefx;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class Application extends javafx.application.Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("sign_in_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Pass-Store");
+        stage.getIcons().add(new Image("file:C:\\Users\\Niall\\OneDrive - University of Limerick\\Desktop\\pass-store-fx\\src\\main\\resources\\com\\niallsom\\passstorefx\\Pass-Store.png"));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
